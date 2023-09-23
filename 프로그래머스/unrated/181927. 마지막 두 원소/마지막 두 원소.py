4f98f54721634d56cd0a726be6b35ce06161f808
Seokwon Lee <120448221+SwLee7@users.noreply.github.com>
2023-09-23 22:03:15 +0900

    [unrated] Title: 마지막 두 원소, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181927. 마지막 두 원소/마지막 두 원소.py
+++ 프로그래머스/unrated/181927. 마지막 두 원소/마지막 두 원소.py
@@ -1,12 +1,3 @@
 def solution(num_list):
-    answer = []
-    for i in range(len(num_list)):
-        answer.append(num_list[i])
-    for i in range(len(answer)):
-        if answer[-1] > answer[-2]:
-            answer.append(answer[-1] - answer[-2])
-            break
-        else : 
-            answer.append(int(answer[-1]) * 2)
-            break
-    return answer
+    num_list.append(num_list[-1]-num_list[-2] if num_list[-1] > num_list[-2] else num_list[-1]*2)
+    return num_list
