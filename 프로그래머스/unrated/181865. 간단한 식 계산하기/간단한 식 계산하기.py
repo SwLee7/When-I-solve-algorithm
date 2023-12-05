ea70da6dae44955cce8bdc968e79bdcced8f340e
Seokwon Lee <120448221+SwLee7@users.noreply.github.com>
2023-12-05 19:50:02 +0900

    [level unrated] Title: 간단한 식 계산하기, Time: 0.03 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181865. 간단한 식 계산하기/간단한 식 계산하기.py
@@ -0,0 +1,11 @@
+def solution(binomial):
+    arr = binomial.split(" ")
+    for i in range(len(arr)):
+        if arr[1] == "+":
+            return int(arr[0]) + int(arr[2])
+        elif arr[1] == "-":
+            return int(arr[0]) - int(arr[2])
+        elif arr[1] == "*":
+            return int(arr[0]) * int(arr[2])
+        elif arr[1] == "/":
+            return int(arr[0]) / int(arr[2])
