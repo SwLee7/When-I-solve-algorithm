587774627119b94e2453a93a9f42f9858c9b5d7b
Seokwon Lee <120448221+SwLee7@users.noreply.github.com>
2024-02-02 22:56:32 +0900

    [level 1] Title: 소수 만들기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.py
@@ -0,0 +1,15 @@
+from itertools import combinations
+
+def solution(nums):
+    answer = 0
+    for i in combinations(nums, 3):
+        s = sum(i)
+        check = True
+        for j in range(2, int(s ** 0.5) + 1):
+            if s % j == 0:
+                check = False
+                break
+        if check:
+            answer += 1
+    return answer
+
